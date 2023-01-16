b71a896d29393c90e8f4ca6d894514797e07d08a
cecilieeps <csande993@gmail.com>
2023-01-16 19:14:46 +0100

    Implement palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function(xs) {
+    // First strip punctuation and space from xs
+    let reverse = [];
+    const charArray = xs.split("").filter(x => x.toLowerCase() !== x.toUpperCase())
+    for (let i = charArray.length - 1; i >= 0; i--) {
+        reverse.push(charArray[i]);
+    }
+    let i = charArray.length-1;
+    while (i--) {
+        if (charArray[i].toLowerCase() !== reverse[i].toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
